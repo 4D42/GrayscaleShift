05ade95cf42a12f403648a72a463777b53a4aafc
Mbaillot <9715332+4D42@users.noreply.github.com>
2018-11-03 23:56:29 +0100

    Add files via upload

---
+++ GrayscallShift.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Commentaires blabalbablabablabl
+
+DigitalVibrance=`nvidia-settings -q [DPY:LVDS-0]/DigitalVibrance -t`
+
+
+if [ $DigitalVibrance != 0 ]
+    then nvidia-settings -a [DPY:LVDS-0]/DigitalVibrance=0
+    else nvidia-settings -a [DPY:LVDS-0]/DigitalVibrance=-1024
+fi
+
